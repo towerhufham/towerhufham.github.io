58b061fa6766690820df95d3392710bf4350a058
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-08 23:54:24 -0500

    Progress on generation

--- tamagotchi.js
+++ tamagotchi.js
@@ -29,7 +29,7 @@ function proceduralDigimon() {
 		closest.seed = null;
 		closest.dist = 999;
 		for (var i = 0; i < seeds.length; i++) {
-			var seed = seeds[i]
+			var seed = seeds[i];
 			var d = distance(seed.x, seed.y, px, py);
 			if (d < closest.dist) {
 				closest.seed = seed;
@@ -39,191 +39,126 @@ function proceduralDigimon() {
 		return closest;
 	}
 	
-	function voronoi(seeds) {
-		//seed should be a list of objects with 
-		//seed.x, seed.y, seed.color
-		for (var y = 0; y < 16; y++) {
-			for (var x = 0; x < 16; x++) {
-				var closest = closestSeed(x, y, seeds);
-				//paint pixel
+	function findFarthestPoints(n, seeds) {
+		//returns the n farthest points away from the given seeds
+		var points = [];
+		for (var i = 0; i < n; i++) {
+			var furthest = {};
+			furthest.dist = 0;
+			furthest.x = null;
+			furthest.y = null;
+			for (var y = 0; y < 16; y++) {
+				for (var x = 0; x < 16; x++) {
+					var nearest = closestSeed(x, y, seeds);
+					var d = distance(nearest.seed.x, nearest.seed.y, x, y);
+					//console.log("d is " + d);
+					if (d > furthest.dist) {
+						furthest.x = x;
+						furthest.y = y;
+						furthest.dist = d;
+						//console.log("new furthest is " + furthest.dist);
+					}
+				}
 			}
+			console.log("furthest found to be " + furthest.dist);
+			seeds.push(furthest); //this line may not work
+			points.push(furthest);
 		}
+		return points;
 	}
 	
 	//note: functions initMon() and loadDigimon() are located in digimon_loader.js
 
-	function getRandomWhitePixel(digimon) {
-		var x = randInt(0,17);
-		var y = randInt(0,17);
-		while (digimon[x][y] == 1) {
-			var x = randInt(0,17);
-			var y = randInt(0,17);
-		}
-		var white = {};
-		white.x = x;
-		white.y = y;
-		return white;
-	}
-
 	function generateTama(tama1, tama2, numberOfSeeds) {
 		var newTama = initMon(16);
-	}
-	
-	function generateDigimon(mon1, mon2, SEEDS=2) {
-		// init
-		var completed = false;
-		var newMon = initMon(18);
-	  
-		//find pixels common to both sourcemons
-		for (var y = 0; y < 18; y++) {
-			for (var x = 0; x < 18; x++) {
-				if (mon1[x][y] === mon2[x][y]) {
-					newMon[x][y] = mon1[x][y];
+		var overlapSeeds = [];
+		
+		//find pixels common to both sourcetamas
+		for (var y = 0; y < 16; y++) {
+			for (var x = 0; x < 16; x++) {
+				//console.log(x+","+y);
+				if (tama1[x][y] === tama2[x][y]) {
+					newTama[x][y] = tama1[x][y];
+					var s = {};
+					s.x = x;
+					s.y = y;
+					overlapSeeds.push(s);
 				}
 			}
 		}
-	  
-		//randomly seed with colors
-		for (var i = 0; i < SEEDS; i++)
-		{
-			var white = getRandomWhitePixel(newMon);
-			newMon[white.x][white.y] = "red";
-			var white = getRandomWhitePixel(newMon);
-			newMon[white.x][white.y] = "blue";
-		}
-	  
-		//debug:
-		//return newMon
-	  
-		//spread colors until the digimon has no white
-		var NUMBER_OF_TRIES = 10;
-		for (var t = 0; t < NUMBER_OF_TRIES; t++)
-		{
-			//find pixels to change
-			var newpixels = [];
-			var wcount = 0;
-			for (var y = 0; y < 18; y++)
-			{
-				for (var x = 0; x < 18; x++) 
-				{
-					if (newMon[x][y] === 0)
-					{
-						wcount++;
-						var c = chooseColorByBoundary(newMon, x, y);
-						if (c != null) {
-							var pix = {}
-							pix.x = x;
-							pix.y = y;
-							pix.c = c;
-							newpixels.push(pix);
-						}
-					}
-				}
-			}
 		
-			//actually change the pixels
-			for (var i = 0, len = newpixels.length; i < len; i++) {
-				var thispix = newpixels[i];
-				newMon[thispix.x][thispix.y] = thispix.c;
-			}
+		//find the farthest 2 points
+		furthest2 = findFarthestPoints(2, overlapSeeds);
 		
-			//end if all white is gone
-			if (wcount === 0) {
-				completed = true;
-				break;
-			}
-		}
-	  
-		//if we got to this point but there are still white pixels, just leave them white
-	  
-		//convert the colors to their proper pixels
-		for (var y = 0; y < 18; y++)
-		{
-			for (var x = 0; x < 18; x++) 
-			{
-				if (newMon[x][y] === "red") {newMon[x][y] = mon1[x][y];}
-				else if (newMon[x][y] === "blue") {newMon[x][y] = mon2[x][y];}
-			}
-		}
-	  
-		return newMon;
+		//paint one red, one blue
+		newTama[furthest2[0].x][furthest2[0].y] = "red";
+		newTama[furthest2[1].x][furthest2[1].y] = "blue";
+		
+		return newTama;
 	}
 	
-	function chooseColorByBoundary(digimon, x, y) {
-		var rcount = 0;
-		var bcount = 0;
+	//for debugging
+	function generateVoronoi() {
+		var v = initMon(16);
+		var seeds = [];
 		
-		//get adjacent pixels
-		if (x > 0) {
-			if (digimon[x-1][y] === "red") {
-				rcount++;
-			} else if (digimon[x-1][y] === "blue") {
-				bcount++;
-			}
-		}
-	  
-		if (x < 17) {
-			if (digimon[x+1][y] === "red") {
-				rcount++;
-			} else if (digimon[x+1][y] === "blue") {
-				bcount++;
-			}
-		}
-	  
-		if (y > 0) {
-			if (digimon[x][y-1] === "red") {
-				rcount++;
-			} else if (digimon[x][y-1] === "blue") {
-				bcount++;
-			}
-		}
-	  
-		if (y < 17) {
-			if (digimon[x][y-1] === "red") {
-				rcount++;
-			} else if (digimon[x][y+1] === "blue") {
-				bcount++;
-			}
-		}
-	  
-		//if there are no adjacent colored pixels, wait until there are
-		if (rcount === 0 && bcount === 0) {
-			return null;
-		}
-		  
-		//if there is a majority, return that
-		else 
-		{
-			if (rcount > bcount) {return "red";}
-			else if (rcount < bcount) {return "blue";}
+		var rseed = {};
+		rseed.x = randInt(0,16);
+		rseed.y = randInt(0,16);
+		rseed.color = "red";
+		seeds.push(rseed);
 		
-			// if it's a tie, choose randomly
-			else if (rcount === bcount) {
-				if (randInt(0,1) === 0) {return "red";}
-				else {return "blue";}
+		var bseed = {};
+		bseed.x = randInt(0,16);
+		bseed.y = randInt(0,16);
+		bseed.color = "blue";
+		seeds.push(bseed);
+		
+		var rseed2 = {};
+		rseed2.x = randInt(0,16);
+		rseed2.y = randInt(0,16);
+		rseed2.color = "red";
+		seeds.push(rseed2);
+		
+		//fill
+		for (var y = 0; y < 16; y++) {
+			for (var x = 0; x < 16; x++) {
+				v[x][y] = closestSeed(x, y, seeds).seed.color;
 			}
 		}
+		
+		//show seeds
+		v[rseed.x][rseed.y] = 1;
+		v[bseed.x][bseed.y] = 1;
+		v[rseed2.x][rseed2.y] = 1;
+		
+		//draw furthest points from seeds
+		var furthests = findFarthestPoints(2, seeds);
+		v[furthests[0].x][furthests[0].y] = 0
+		v[furthests[1].x][furthests[1].y] = 0
+		
+		return v;
 	}
 	
-	function drawDigimon(digimon) {
-		for (var i = 0; i < 18; i++) 
+	function drawTama(tama) {
+		for (var i = 0; i < 16; i++) 
 		{
-			for (var j = 0; j < 18; j++) 
+			for (var j = 0; j < 16; j++) 
 			{
-				if (digimon[i][j] === 1) {
+				if (tama[i][j] === 1) {
 					drawPixel(i,j);
 				}
-				else if (digimon[i][j] === "red") {
+				else if (tama[i][j] === "red") {
 					drawPixel(i,j,color="red");
 				}
-				else if (digimon[i][j] === "blue") {
+				else if (tama[i][j] === "blue") {
 					drawPixel(i,j,color="blue");
 				}
 			}
 		}
 	}
-	
-	//generate the digimon
+
+	//generate the tama
 	ctx.fillStyle="#FFFFFF";
 	ctx.fillRect(0,0,324,324);
 	
@@ -238,6 +173,6 @@ function proceduralDigimon() {
 
 	mon1 = loadDigimon(seed1);
 	mon2 = loadDigimon(seed2);
-	newMon = generateDigimon(mon1, mon2);
-	drawDigimon(newMon);
+	newMon = generateVoronoi(mon1, mon2);
+	drawTama(newMon);
 }
