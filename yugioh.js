a281f7b60a073ab0b0a66a3afe9d82df284324fc
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-22 18:37:02 -0500

    Implemented file downloading

--- yugioh.js
+++ yugioh.js
@@ -40,11 +40,31 @@ function getRandomID() {
 	return getIDFromName(randomCardName())
 }
 
-function init() {
+function createDeckContent() {
+	var content = "#created by random chance at towerhufham.com\n#main\n";
 	for (var i = 0; i < 40; i++) {
-		console.log(randomMainDeckID())
+		content += randomMainDeckID() + "\n";
 	}
-	// for (var i = 0; i < 15; i++) {
-		// console.log(randomExtraDeckID())
-	// }
+	return content;
+}
+
+// from https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
+function download(filename, text) {
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
+
+	element.style.display = 'none';
+	document.body.appendChild(element);
+
+	element.click();
+
+	document.body.removeChild(element);
+}
+
+function init() {
+	var content = createDeckContent();
+	// uriContent = "data:application/octet-stream," + encodeURIComponent(content);
+	// location.href = uriContent
+	download("random-deck.ydk", content);
 }
