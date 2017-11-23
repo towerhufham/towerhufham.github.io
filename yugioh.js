04e0f39f7e94a992976b632397c4d85857aa97c9
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-23 18:13:56 -0500

    Started converting to jQuery
    (very poorly)

--- yugioh.js
+++ yugioh.js
@@ -1,49 +1,53 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.ygohub.com/api/all_cards", false);
-xhr.send()
-var cards = JSON.parse(xhr.response)["cards"]
+// var xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://www.ygohub.com/api/all_cards", false);
+// xhr.send()
+// var cards = JSON.parse(xhr.response)["cards"]
+
+function getCardList(callback) {
+	$.getJSON("https://www.ygohub.com/api/all_cards", function(data) {
+		callback(data["cards"]);
+	})
+}
 
 function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomCardName() {
+function randomCardName(cards) {
 	return cards[randInt(0, cards.length)];
 }
 
-function randomMainDeckID() {
-	var card = getCard(randomCardName());
-	while (card["is_extra_deck"]) {
-		//console.log("found an extra deck card, retrying")
-		card = getCard(randomCardName());
-	}
-	return card["number"]
+function randomMainDeckID(cards, callback) {
+	// var card = getCard(randomCardName());
+	// while (card["is_extra_deck"]) {
+		// card = getCard(randomCardName());
+	// }
+	// return card["number"]
+	getCard(randomCardName(cards), function(card) {
+		console.log(card["number"]);
+		callback(card["number"]);
+	});
 }
 
-function randomExtraDeckID() {
-	var card = getCard(randomCardName());
-	while (!(card["is_extra_deck"])) {
-		//console.log("found a main deck card, retrying")
-		card = getCard(randomCardName());
-	}
-	return card["number"]
-}
-
-function getCard(name) {
-	xhr.open("GET", "https://www.ygohub.com/api/card_info?name="+name, false);
-	xhr.send();
-	var data = JSON.parse(xhr.response);
-	return data["card"]
+function getCard(name, callback) {
+	// xhr.open("GET", "https://www.ygohub.com/api/card_info?name="+name, false);
+	// xhr.send();
+	// var data = JSON.parse(xhr.response);
+	// return data["card"]
+	
+	$.getJSON("https://www.ygohub.com/api/card_info?name="+name, function(data) {
+		callback(data["card"]);
+	});
 }
 
 function getRandomID() {
 	return getIDFromName(randomCardName())
 }
 
-function createDeckContent() {
+function createDeckContent(cards) {
 	var content = "#created by random chance at towerhufham.com\n#main\n";
 	for (var i = 0; i < 40; i++) {
-		content += randomMainDeckID() + "\n";
+		content += randomMainDeckID(cards) + "\n";
 	}
 	return content;
 }
@@ -63,8 +67,23 @@ function download(filename, text) {
 }
 
 function init() {
-	var content = createDeckContent();
-	// uriContent = "data:application/octet-stream," + encodeURIComponent(content);
-	// location.href = uriContent
-	download("random-deck.ydk", content);
+	// var content = createDeckContent();
+	// download("random-deck.ydk", content);
+	// testing
+	getCardList(function(cards) {
+		getCard(randomCardName(cards), function(card){console.log(card);});
+		list = [];
+		for (var i = 0; i < 40; i++) {
+			// randomMainDeckID(cards, function(id){console.log(id);});
+			randomMainDeckID(cards, function(id){list.push(id);});
+		}
+		
+		var timeout = setInterval(function() {
+			if(list.length >= 40) {
+				clearInterval(timeout); 
+				console.log(list);
+			} 
+		}, 100);
+		//setTimeout(function() {console.log(list);}, 2000);
+	});
 }
