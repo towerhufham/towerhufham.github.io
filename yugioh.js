eab657db462e4b716a746b6e788c9087b7016822
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-24 16:49:36 -0500

    Progress on keeping invalid cards out of deck

--- yugioh.js
+++ yugioh.js
@@ -14,16 +14,47 @@ function getCardList(callback) {
 	});
 }
 
-function randomID(cards) {
+/* function randomID(cards) {
 	return new Promise((resolve, reject) => {
 		//this syntax is basically the long way of using "await". This is helpful: https://stackoverflow.com/questions/46159415/await-promises-all-syntaxerror
 		var card = getCard(randomCardName(cards)).then((card) => resolve(card["number"]));
 	});
+} */
+
+async function randomID(cards) {
+	var card = await randomCard(cards);
+	return card["number"];
+}
+
+function randomCard(cards) {
+	//NOTE: sometimes returns undefined, I suspect it's an issue with the ygohub api.
+	return new Promise((resolve, reject) => {
+		//this syntax is basically the long way of using "await". This is helpful: https://stackoverflow.com/questions/46159415/await-promises-all-syntaxerror
+		var card = getCard(randomCardName(cards)).then((card) => resolve(card));
+	});
+}
+
+async function randomMainDeckID(cards) {
+	function isValid(card) {
+		if (!(typeof card === "undefined")) {
+			if (!(card["is_extra_deck"] || isNaN(card["number"]))) {
+				return true;
+			}
+		}
+		return false;
+	}
+	var card = await randomCard(cards);
+	while (!(isValid(card))) {
+		card = await randomCard(cards);
+	}
+	return card["number"]
 }
 	
 function getCard(name) {
 	return new Promise((resolve, reject) => {
-		$.getJSON("https://www.ygohub.com/api/card_info?name="+name, function(data) {resolve(data["card"]);})
+		$.getJSON("https://www.ygohub.com/api/card_info?name="+name, function(data) {
+			resolve(data["card"]);
+		})
 	});
 }
 
@@ -31,7 +62,8 @@ async function createDeckContent() {
 	var cards = await getCardList();
 	var content = "#created by random chance at towerhufham.com\n#main\n";
 	for (var i = 0; i < 40; i++) {
-		var id = await randomID(cards);
+		//var id = await randomID(cards);
+		var id = await randomMainDeckID(cards);
 		content += id + "\n";
 	}
 	return content;
