e91295b22aa338d24519bcda43cf67e0926987cd
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-23 19:17:51 -0500

    Converting callbacks to async/await functions
    it's rolling

--- yugioh.js
+++ yugioh.js
@@ -4,9 +4,15 @@
 // var cards = JSON.parse(xhr.response)["cards"]
 
 function getCardList(callback) {
-	$.getJSON("https://www.ygohub.com/api/all_cards", function(data) {
-		callback(data["cards"]);
-	})
+	// $.getJSON("https://www.ygohub.com/api/all_cards", function(data) {
+		// callback(data["cards"]);
+	// })
+	
+	return new Promise((resolve, reject) => {
+		$.getJSON("https://www.ygohub.com/api/all_cards", function(data) {
+			resolve(data["cards"]);
+		})
+	});
 }
 
 function randInt(min, max) {
@@ -17,26 +23,34 @@ function randomCardName(cards) {
 	return cards[randInt(0, cards.length)];
 }
 
-function randomMainDeckID(cards, callback) {
+function randomMainDeckID(cards) {
 	// var card = getCard(randomCardName());
 	// while (card["is_extra_deck"]) {
 		// card = getCard(randomCardName());
 	// }
 	// return card["number"]
-	getCard(randomCardName(cards), function(card) {
-		console.log(card["number"]);
-		callback(card["number"]);
+	return new Promise((resolve, reject) => {
+		// getCard(randomCardName(cards), function(card) {
+			// console.log(card["number"]);
+			// callback(card["number"]);
+		// });
+		//this syntax is basically the long way of using "await". This is helpful: https://stackoverflow.com/questions/46159415/await-promises-all-syntaxerror
+		var card = getCard(randomCardName(cards)).then((card) => resolve(card["number"]));
 	});
 }
 
-function getCard(name, callback) {
+function getCard(name) {
 	// xhr.open("GET", "https://www.ygohub.com/api/card_info?name="+name, false);
 	// xhr.send();
 	// var data = JSON.parse(xhr.response);
 	// return data["card"]
 	
-	$.getJSON("https://www.ygohub.com/api/card_info?name="+name, function(data) {
-		callback(data["card"]);
+	// $.getJSON("https://www.ygohub.com/api/card_info?name="+name, function(data) {
+		// callback(data["card"]);
+	// });
+	
+	return new Promise((resolve, reject) => {
+		$.getJSON("https://www.ygohub.com/api/card_info?name="+name, function(data) {resolve(data["card"]);})
 	});
 }
 
@@ -50,6 +64,16 @@ function createDeckContent(cards) {
 		content += randomMainDeckID(cards) + "\n";
 	}
 	return content;
+}
+
+async function createDeckContent() {
+	var cards = await getCardList();
+	var list = [];
+	for (var i = 0; i < 40; i++) {
+		var id = await randomMainDeckID(cards);
+		list.push(id);
+	}
+	console.log(list);
 }
 
 // from https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
@@ -86,15 +110,18 @@ function init() {
 		// }, 100);
 	// });
 	
+	//testing 2
+	createDeckContent();
+	
 	//async kiddie-pool
-	function getTextSlowly(text) {
-		return new Promise((resolve, reject) => {
-			setTimeout(function() {resolve(text+"...");}, 2000);
-		});
-	}
-	async function doIt() {
-		var p = await getTextSlowly("did it work?");
-		console.log(p);
-	}
-	doIt();
+	// function getTextSlowly(text) {
+		// return new Promise((resolve, reject) => {
+			// setTimeout(function() {resolve(text+"...");}, 2000);
+		// });
+	// }
+	// async function doIt() {
+		// var p = await getTextSlowly("did it work?");
+		// console.log(p);
+	// }
+	// doIt();
 }
