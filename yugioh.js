17bc0c2058744d9194c2dc858b2cbb13bb6b12bc
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-23 20:13:25 -0500

    Reimplemented downloading

--- yugioh.js
+++ yugioh.js
@@ -14,35 +14,27 @@ function getCardList(callback) {
 	});
 }
 
-function randomMainDeckID(cards) {
+function randomID(cards) {
 	return new Promise((resolve, reject) => {
 		//this syntax is basically the long way of using "await". This is helpful: https://stackoverflow.com/questions/46159415/await-promises-all-syntaxerror
 		var card = getCard(randomCardName(cards)).then((card) => resolve(card["number"]));
 	});
 }
-
+	
 function getCard(name) {
 	return new Promise((resolve, reject) => {
 		$.getJSON("https://www.ygohub.com/api/card_info?name="+name, function(data) {resolve(data["card"]);})
 	});
 }
 
-// function createDeckContent(cards) {
-	// var content = "#created by random chance at towerhufham.com\n#main\n";
-	// for (var i = 0; i < 40; i++) {
-		// content += randomMainDeckID(cards) + "\n";
-	// }
-	// return content;
-// }
-
 async function createDeckContent() {
 	var cards = await getCardList();
-	var list = [];
+	var content = "#created by random chance at towerhufham.com\n#main\n";
 	for (var i = 0; i < 40; i++) {
-		var id = await randomMainDeckID(cards);
-		list.push(id);
+		var id = await randomID(cards);
+		content += id + "\n";
 	}
-	console.log(list);
+	return content;
 }
 
 // from https://ourcodeworld.com/articles/read/189/how-to-create-a-file-and-generate-a-download-with-javascript-in-the-browser-without-a-server
@@ -60,9 +52,10 @@ function download(filename, text) {
 }
 
 function init() {
-	// var content = createDeckContent();
-	// download("random-deck.ydk", content);
-	createDeckContent();
+	createDeckContent().then((content) => {
+		console.log(content);
+		download("random-deck.ydk", content);
+	});
 	
 	//async kiddie-pool
 	// function getTextSlowly(text) {
