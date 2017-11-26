2b03f03608eea34f0e04cf7fc6fe3e77454b533a
towerhufham <32599109+towerhufham@users.noreply.github.com>
2017-11-25 23:55:18 -0500

    Implemented related card searches
    took a lot more debugging than i thought, too

--- yugioh.js
+++ yugioh.js
@@ -23,30 +23,90 @@ function randomCard(cards) {
 	});
 }
 
-async function randomMainDeckID(cards) {
-	function isValid(card) {
-		if (!(typeof card === "undefined")) {
-			if (!(card["is_extra_deck"] || isNaN(card["number"]))) {
-				return true;
+function getRelatedCards(card) {
+	//this function returns a list of related cards to search for later
+	//this regex gets all text withtin (and including) quotes, which is used in card text to refer to archeytpes and specific cards
+	var matches = card["text"].match(/"(.*?)"/g);
+	var related  = [];
+	//if there were any quotes
+	if (matches) {
+		//check every match (we skip matches[0], as it's the match itself instead of a captured part of the match)
+		for (var i = 1; i < matches.length; i++) {
+			//remove the quotes from the match
+			var match = matches[i].replace(/"/g, "");
+			//if remove "(s)" from the match
+			var match = match.replace(/\(s\)/g, "");
+			//cards refer to themselves within quotes, which we don't want
+			//tokens are also referred to with quotes, which we don't want either
+			if (match != card["name"] && !(match.includes("Token"))) {
+				related.push(match);
 			}
 		}
-		return false;
 	}
+	//remove all duplicate references by converting to a set and back
+	related = Array.from(new Set(related));
+	//console.log(related);
+	return related;
+}
+
+function isValid(card) {
+	if (!(typeof card === "undefined")) {
+		if (!(card["is_extra_deck"] || isNaN(card["number"]))) {
+			return true;
+		}
+	}
+	return false;
+}
+
+async function randomMainDeckID(cards) {
 	var card = await randomCard(cards);
 	while (!(isValid(card))) {
 		card = await randomCard(cards);
 	}
 	
 	//check for related cards
-	var matches = card["text"].match(/"(.*?)"/);
-	if (matches) {
-		//TODO: make this check for multiple matches
-		if (matches[1] != card["name"] && !(matches[1].includes("Token"))) {
-			console.log(matches[1]);
+	var related = getRelatedCards(card);
+	
+	return {id:card["number"], related:related};
+}
+	
+function getNamesContaining(cards, substring) {
+	names = [];
+	for (var i = 0; i < cards.length; i++) {
+		if (cards[i].includes(substring)) {
+			names.push(cards[i]);
 		}
 	}
+	return names;
+}
 	
-	return card["number"]
+async function randomRelatedCard(cards, related) {
+	var names = getNamesContaining(cards, related);
+	if (names.length > 0) {
+		var name = names[randInt(0, names.length)];
+		removeValue(names, name);
+		var card = await getCard(name);
+		//try every name until one is valid
+		while (!(isValid(card))) {
+			if (names.length > 0) {
+				var name = names[randInt(0, names.length)];
+				removeValue(names, name);
+				var card = await getCard(name);
+			} else {
+				//if we get here, we've exhausted all possibilities
+				console.log("Exhausted all possibilities");
+				break;
+			}
+		}
+		if (isValid(card)) {
+			console.log("Searched for "+related+", got "+card["name"]+", id="+card["number"]+".");
+			var newRelated = getRelatedCards(card);
+			return {id:card["number"]};
+		}
+	}
+	//for now, if the card is invalid just call it a loss and get a random card. TODO make this better maybe?
+	console.log("Searched for "+related+", couldn't find anything valid.");
+	return randomMainDeckID(cards);
 }
 	
 function getCard(name) {
@@ -57,19 +117,58 @@ function getCard(name) {
 	});
 }
 
+function removeValue(arr, val) {
+	var index = arr.indexOf(val);
+	if (index > -1) {
+		arr.splice(index, 1);
+	}
+	return arr;
+}
+
 async function createDeckContent() {
 	//init
 	var cards = await getCardList();
 	var decklist = new Array(40).fill(null);
+	//this magically makes a list of 0 to 40
+	var avalibleSlots = [...Array(40).keys()]
 	
 	//fill decklist with ids
 	for (var i = 0; i < 40; i++) {
-		var id = await randomMainDeckID(cards);
-		decklist[i] = id;
+		if (decklist[i] == null) {
+			//get card id
+			var card = await randomMainDeckID(cards);
+			var id = card.id;
+			//write the id to this slot
+			decklist[i] = id;
+			//this slot is no longer avalible
+			removeValue(avalibleSlots, i);
+			//put the related words in random unused spots in the decklist
+			var related = card.related;
+			for (var j = 0; j < related.length; j++) {
+				var index = avalibleSlots[randInt(0, avalibleSlots.length)]
+				removeValue(avalibleSlots, index);
+				decklist[index] = related[j];
+				//console.log("Current decklist: "+decklist);
+			}
+		//if decklist[i] is text, search for it in related cards
+		} else if (isNaN(decklist[i])) {
+			//get related card
+			console.log("Looking for related cards, i="+i+".");
+			var card = await randomRelatedCard(cards, decklist[i]);
+			//write the id to this slot (don't have to remove from avalibleSlots because it has already been remove)
+			decklist[i] = card.id;
+			//put the related words in random unused spots in the decklist, the same as above
+			var related = card.related;
+			if (related) {
+				for (var j = 0; j < related.length; j++) {
+					var index = avalibleSlots[randInt(0, avalibleSlots.length)]
+					removeValue(avalibleSlots, index);
+					decklist[index] = related[j];
+					console.log("Current decklist: "+decklist);
+				}
+			}
+		}
 	}
-	
-	//TODO: card requests
-	
 	//create content string
 	var content = "#created by random chance at towerhufham.com\n#main\n";
 	for (var i = 0; i < 40; i++) {
@@ -86,7 +185,7 @@ function setDownloadHref(filename, text) {
 	img.src = "img/yugioh_download.png";
 }
 
-function init() {
+function init() {	
 	createDeckContent().then((content) => {
 		console.log(content);
 		setDownloadHref("random-deck.ydk", content);
